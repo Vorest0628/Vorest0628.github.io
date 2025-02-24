51c326b7e7dc3eb3c0b65f56cae2a3b86ba22aea
[Vorest0628] <GeodgeChen@outlook.com>
2025-02-24 21:34:22 +0800

    java modify

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,29 @@ myImage.onclick = () => {
   }
 };
 
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Ciallo～(∠・ω< )⌒★, ${myName}`;
+  }
+}
+
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Ciallo～(∠・ω< )⌒★, ${storedName}`;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
+
+
