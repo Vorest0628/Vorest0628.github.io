47bd087a253f6466dcc2081b5cb7049af4ede637
[Vorest0628] <GeodgeChen@outlook.com>
2025-03-21 19:26:20 +0800

    about

---
+++ about/mainjs.js
@@ -0,0 +1,13 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "image/00007.png") {
+    myImage.setAttribute("src", "../image/00002.png");
+  } else {
+    myImage.setAttribute("src", "../image/00007.png");
+  }
+};
+
+
+
